37b336c740f2e9a2eb7e6b2e952db8dfd2fca438
nobi1007 <mittalshyam1007@gmail.com>
2020-02-12 00:12:48 +0530

    second commit

---
+++ app/src/main/java/com/example/skincancerdetection/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.skincancerdetection;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btLogin, btRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btLogin = (Button)findViewById(R.id.buttonLogin);
+        btRegister = (Button)findViewById(R.id.buttonRegister);
+        buttonClickListener();
+    }
+
+    public void buttonClickListener(){
+        btLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentLogin = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intentLogin);
+            }
+        });
+
+        btRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentLogin = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intentLogin);
+            }
+        });
+    }
+
+}
