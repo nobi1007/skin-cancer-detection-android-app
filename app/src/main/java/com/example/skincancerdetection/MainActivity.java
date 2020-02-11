24e8876ed3e47498196035367df2a1590d877fc4
nobi1007 <mittalshyam1007@gmail.com>
2020-02-12 01:57:44 +0530

    login and register interface done

--- app/src/main/java/com/example/skincancerdetection/MainActivity.java
+++ app/src/main/java/com/example/skincancerdetection/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Home");
         btLogin = (Button)findViewById(R.id.buttonLogin);
         btRegister = (Button)findViewById(R.id.buttonRegister);
         buttonClickListener();
